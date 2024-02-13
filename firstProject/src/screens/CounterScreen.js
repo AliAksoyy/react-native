95f2cd2591604d1486ff63891747f8daac3dd2d4
AliAksoyy <alifeyza4017@gmail.com>
2024-02-14 00:40:45 +0300

    added

--- firstProject/src/screens/CounterScreen.js
+++ firstProject/src/screens/CounterScreen.js
@@ -1,24 +1,39 @@
 import { StyleSheet, Text, View, Button } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
+
+const reducer = (state, { type, payload }) => {
+  switch (type) {
+    case "increment":
+      return { ...state, counter: state.counter + payload };
+
+    case "decrement":
+      return { ...state, counter: state.counter - payload };
+    default:
+      return state;
+  }
+};
 
 const CounterScreen = () => {
-  const [counter, setCounter] = useState(0);
+  // const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { counter: 0 });
 
   return (
     <View>
       <Button
         title="Arttır"
         onPress={() => {
-          setCounter(counter + 1);
+          dispatch({ type: "increment", payload: 1 });
+          // setCounter(counter + 1);
         }}
       />
       <Button
         title="Azalt"
         onPress={() => {
-          setCounter(counter - 1);
+          // setCounter(counter - 1);
+          dispatch({ type: "decrement", payload: 1 });
         }}
       />
-      <Text>Sayı {counter}</Text>
+      <Text>Sayı {state.counter}</Text>
     </View>
   );
 };
