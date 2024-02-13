0b5cc5752de82245a979aa65bb0ae043df94d1b8
AliAksoyy <alifeyza4017@gmail.com>
2024-02-13 21:38:59 +0300

    added

--- firstProject/App.js
+++ firstProject/App.js
@@ -1,27 +1,33 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 
 export default function App() {
+  const courses = [
+    { name: "Angular", id: 1 },
+    { name: "React JS", id: 2 },
+    { name: "C#", id: 3 },
+    { name: "C Programlama", id: 4 },
+    { name: "Bootstrap", id: 5 },
+  ];
+
   return (
     <>
-      <View style={styles.container}>
-        <Text style={styles.title}>React Native</Text>
-        <Text style={styles.content}>React JS</Text>
-      </View>
+      <FlatList
+        data={courses}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => {
+          return <Text style={styles.content}>{item.name}</Text>;
+        }}
+      />
     </>
   );
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 50,
-  },
   content: {
     fontSize: 20,
+    backgroundColor: "yellow",
+    marginVertical: 10,
+    padding: 20,
   },
 });
