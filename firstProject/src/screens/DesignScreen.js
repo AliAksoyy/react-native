75c90dedeefe193a2a7e16d2d001d0ea4af898bf
AliAksoyy <alifeyza4017@gmail.com>
2024-02-14 08:28:17 +0300

    added

--- firstProject/src/screens/DesignScreen.js
+++ firstProject/src/screens/DesignScreen.js
@@ -3,12 +3,52 @@ import React from "react";
 
 export default function DesignScreen() {
   return (
-    <View>
-      <View style={{ width: 50, height: 50, backgroundColor: "red" }} />
-      <View style={{ width: 100, height: 100, backgroundColor: "blue" }} />
-      <View style={{ width: 150, height: 150, backgroundColor: "green" }} />
+    <View style={styles.mainView}>
+      <Text style={styles.mainTextOne}>1</Text>
+      {/* <Text style={styles.mainTextTwo}>2</Text>
+      <Text style={styles.mainTextThree}>3</Text> */}
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mainView: {
+    borderWidth: 1,
+    borderColor: "black",
+    margin: 20,
+    borderRadius: 20,
+    height: 400,
+    // flexDirection: "row",
+  },
+  mainTextOne: {
+    // flex: 4,
+    borderWidth: 1,
+    borderColor: "red",
+    margin: 10,
+    padding: 20,
+    borderRadius: 20,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    // alignSelf: "flex-start",
+  },
+  //   mainTextTwo: {
+  //     // flex: 5,
+  //     borderWidth: 1,
+  //     borderColor: "red",
+  //     margin: 10,
+  //     padding: 20,
+  //     borderRadius: 20,
+  //     top: 10,
+  //     left: 10,
+  //   },
+  //   mainTextThree: {
+  //     // flex: 1,
+  //     borderWidth: 1,
+  //     borderColor: "red",
+  //     margin: 10,
+  //     padding: 20,
+  //     borderRadius: 20,
+  //   },
+});
