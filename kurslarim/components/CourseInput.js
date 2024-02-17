71c619d253026084dc02479b89518434ab6019a1
AliAksoyy <alifeyza4017@gmail.com>
2024-02-17 08:16:54 +0300

    added

--- kurslarim/components/CourseInput.js
+++ kurslarim/components/CourseInput.js
@@ -1,12 +1,40 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  Image,
+  Modal,
+} from "react-native";
 import React from "react";
 
 export default function CourseInput({ visible }) {
   return (
-    <View>
-      <Text>CourseInput</Text>
-    </View>
+    <Modal animationType="slide" visible={visible}>
+      <View style={styles.inputContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/image/images.png")}
+        />
+        <TextInput style={styles.textInput} placeholder="Yeni Kurs Ekle" />
+        <View style={styles.buttonContainer}>
+          <View>
+            <Button style={styles.button} title="iptal ekle" color="red" />
+          </View>
+          <View>
+            <Button style={styles.button} title="Ekle" color="blue" />
+          </View>
+        </View>
+      </View>
+    </Modal>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer: {},
+  image: {},
+  textInput: {},
+  buttonContainer: {},
+  button: {},
+});
