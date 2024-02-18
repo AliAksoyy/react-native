ffecf0787e4afb563bbebcebfa2225e8a8de0eb4
AliAksoyy <alifeyza4017@gmail.com>
2024-02-18 18:02:38 +0300

    added

--- blogsApp/context/BlogContext.js
+++ blogsApp/context/BlogContext.js
@@ -1,8 +1,18 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 const BlogContext = createContext();
 
+const blogReducer = (state, { type, payload }) => {
+  switch (type) {
+    case "add":
+      return state;
+    default:
+      return state;
+  }
+};
 export const BlogProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(blogReducer, []);
+
   return <BlogContext.Provider value="a">{children}</BlogContext.Provider>;
 };
 
