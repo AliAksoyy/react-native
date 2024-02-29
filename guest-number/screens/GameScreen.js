23165d7237cc575fe1086b5304e54830d138d0bb
AliAksoyy <alifeyza4017@gmail.com>
2024-03-01 00:05:45 +0300

    added

--- guest-number/screens/GameScreen.js
+++ guest-number/screens/GameScreen.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import Title from "../components/Title";
 import ComputerNumber from "../components/ComputerNumber";
 import CustomButton from "../components/CustomButton";
+import { AntDesign } from "@expo/vector-icons";
 
 let minNumber = 1;
 let maxNumber = 100;
@@ -51,14 +52,14 @@ export default function GameScreen({ userNumber, onGameOver }) {
     <View style={styles.container}>
       <Title>Bilgisayar Tahmini</Title>
       <ComputerNumber>{currentGuess}</ComputerNumber>
-      <View>
-        <Text>Altında mı üstünde mi ?</Text>
-        <View>
+      <View style={styles.card}>
+        <Text style={styles.title}>Altında mı üstünde mi ?</Text>
+        <View style={styles.buttonsContainer}>
           <CustomButton onPress={nextGuessHandler.bind(this, "lower")}>
-            -
+            <AntDesign name="minus" size={24} color={"white"} />
           </CustomButton>
           <CustomButton onPress={nextGuessHandler.bind(this, "greater")}>
-            +
+            <AntDesign name="plus" size={24} color={"white"} />
           </CustomButton>
         </View>
       </View>
@@ -70,5 +71,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 30,
+  },
+  card: {
+    backgroundColor: "orange",
+    padding: 16,
+    marginTop: 20,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  title: {
+    color: "white",
+    fontSize: 25,
+    marginBottom: 15,
   },
 });
