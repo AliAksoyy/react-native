c0836fcb12fb221871c034f59467e52c2c20172c
AliAksoyy <alifeyza4017@gmail.com>
2024-03-10 17:42:06 +0300

    added

--- auth/util/auth.js
+++ auth/util/auth.js
@@ -3,17 +3,21 @@ import axios from "axios";
 const API_KEY = "AIzaSyAAzxlGmAHZ7lwmGrEjrtFU-_QvlgxdE_A";
 
 async function authenticate(mode, email, password) {
-  const response = await axios.post(
-    `https://identitytoolkit.googleapis.com/v1/accounts:${mode}"?key=${API_KEY}`,
-    { email: email, password: password, returnSecureToken: true }
-  );
+  try {
+    const response = await axios.post(
+      `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`,
+      { email: email, password: password, returnSecureToken: true }
+    );
 
-  console.log("response", response);
+    console.log("response", response);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-export async function createUser(email, password) {
+export function createUser(email, password) {
   return authenticate("signUp", email, password);
 }
-export async function login(email, password) {
+export function login(email, password) {
   return authenticate("signInWithPassword", email, password);
 }
