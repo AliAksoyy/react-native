fc54a463282879f66e126f878856d64f9e77dc9e
AliAksoyy <alifeyza4017@gmail.com>
2024-03-12 01:51:57 +0300

    added

--- food/screens/FoodOverviewScreen.js
+++ food/screens/FoodOverviewScreen.js
@@ -1,11 +1,25 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import React from "react";
+import { FOODS } from "../data/dummy-data";
+import FoodItem from "../components/FoodItem";
 
 export default function FoodOverviewScreen({ route }) {
   const { categoryId } = route.params;
+  const displayFoods = FOODS.filter(
+    (food) => food.categoryIds.indexOf(categoryId) >= 0
+  );
+
+  function renderFoodItem({ item }) {
+    return <FoodItem {...item} />;
+  }
+
   return (
     <View>
-      <Text>FoodOverviewScreen -{categoryId}</Text>
+      <FlatList
+        data={displayFoods}
+        keyExtractor={(item) => item.id}
+        renderItem={renderFoodItem}
+      />
     </View>
   );
 }
