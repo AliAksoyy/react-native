90874e6c0079e5c36ed31ef180f140fc64a50cb6
AliAksoyy <alifeyza4017@gmail.com>
2024-03-12 01:19:26 +0300

    addeded

--- food/App.js
+++ food/App.js
@@ -4,7 +4,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
-
 export default function App() {
   return (
     <NavigationContainer>
