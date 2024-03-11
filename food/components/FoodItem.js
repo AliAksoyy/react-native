262896250ba03cfd14990163d7d62ee6bd6d1596
AliAksoyy <alifeyza4017@gmail.com>
2024-03-12 02:24:46 +0300

    added

--- food/components/FoodItem.js
+++ food/components/FoodItem.js
@@ -1,6 +1,5 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, Pressable } from "react-native";
 import React from "react";
-import { FOODS } from "../data/dummy-data";
 
 export default function FoodItem({
   id,
@@ -10,10 +9,49 @@ export default function FoodItem({
   complexity,
 }) {
   return (
-    <View>
-      <Text>{title}</Text>
-    </View>
+    <Pressable
+      style={({ pressed }) => [styles.container, pressed && styles.pressed]}
+    >
+      <Image
+        style={styles.image}
+        source={{
+          uri: imageUrl,
+        }}
+      />
+      <Text style={styles.title}>{title}</Text>
+      <View style={styles.innerView}>
+        <Text style={styles.subTitle}>{complexity}</Text>
+        <Text style={styles.subTitle}> {affordability}</Text>
+      </View>
+    </Pressable>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 20,
+    backgroundColor: "white",
+    elevation: 4,
+    shadowColor: "#123231",
+    shadowOffset: { width: 2, height: 4 },
+    shadowRadius: 3,
+    shadowOpacity: 0.4,
+    borderRadius: 10,
+    paddingBottom: 10,
+  },
+  pressed: {
+    opacity: 0.6,
+  },
+  image: {
+    width: "100%",
+    height: 250,
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+  },
+  title: { marginTop: 10, fontSize: 19, fontWeight: "bold" },
+  innerView: { flexDirection: "row", columnGap: 10 },
+  subTitle: {},
+});
