0d13f04f2797d68e29d55bfdc33bd0e28347bfb6
AliAksoyy <alifeyza4017@gmail.com>
2024-03-12 00:29:58 +0300

    added

--- food/App.js
+++ food/App.js
@@ -1,8 +1,18 @@
 import { StyleSheet } from "react-native";
 import Categories from "./screens/Categories";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <Categories />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Categories" component={Categories} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
