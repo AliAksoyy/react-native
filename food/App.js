0e6e79118df0694156c93b04d055bbff474f0aeb
AliAksoyy <alifeyza4017@gmail.com>
2024-03-12 11:37:43 +0300

    added

--- food/App.js
+++ food/App.js
@@ -4,8 +4,44 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import FoodOverviewScreen from "./screens/FoodOverviewScreen";
 import FoodDetailScreen from "./screens/FoodDetailScreen";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import FavoritesScreen from "./screens/FavoritesScreen";
+import { Ionicons, AntDesign } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "blue" },
+        headerTintColor: "white",
+      }}
+    >
+      <Drawer.Screen
+        name="Categories"
+        component={Categories}
+        options={{
+          title: "Tüm Kategoriler",
+          drawerIcon: () => {
+            return <Ionicons name="list" size={18} color="blue" />;
+          },
+        }}
+      />
+      <Drawer.Screen
+        name="Favorite"
+        component={FavoritesScreen}
+        options={{
+          title: "Favoriler",
+          drawerIcon: () => {
+            return <AntDesign name="star" size={18} color="blue" />;
+          },
+        }}
+      />
+    </Drawer.Navigator>
+  );
+}
 export default function App() {
   return (
     <NavigationContainer>
@@ -17,9 +53,9 @@ export default function App() {
         }}
       >
         <Stack.Screen
-          name="Categories"
-          component={Categories}
-          options={{ title: "Tüm Kategoriler" }}
+          name="Drawer"
+          component={DrawerNavigator}
+          options={{ headerShown: false }}
         />
         <Stack.Screen name="FoodOverView" component={FoodOverviewScreen} />
         <Stack.Screen
