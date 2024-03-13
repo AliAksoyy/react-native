95735e2075eb9227626166e2d67cf94aaac0cfe2
AliAksoyy <alifeyza4017@gmail.com>
2024-03-14 02:00:38 +0300

    aadded

--- kurslarimDetail/components/CoursesSummary.js
+++ kurslarimDetail/components/CoursesSummary.js
@@ -1,12 +1,31 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-export default function CoursesSummary({ periodName }) {
+export default function CoursesSummary({ courses, periodName }) {
+  const courseSum = courses.reduce((acc, cur) => {
+    return acc + cur.amount;
+  }, 0);
+
   return (
-    <View>
-          <Text>{periodName}</Text>
+    <View style={styles.container}>
+      <Text style={styles.left}>{periodName}</Text>
+      <Text style={styles.right}>{courseSum + " TL"}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingVertical: 10,
+    backgroundColor: "blue",
+    borderRadius: 10,
+    paddingHorizontal: 10,
+  },
+
+  left: { color: "white", fontSize: 12 },
+  right: { color: "white", fontSize: 16, fontWeight: "bold" },
+});
