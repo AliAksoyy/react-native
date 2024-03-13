b4e1be8698b4e0efff2f856a287adea8e7d79f39
AliAksoyy <alifeyza4017@gmail.com>
2024-03-14 00:15:35 +0300

    aadded

--- kurslarimDetail/App.js
+++ kurslarimDetail/App.js
@@ -1,19 +1,60 @@
-import { StyleSheet } from "react-native";
+import { Pressable, StyleSheet, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import AllCourses from "./screens/AllCourses";
 import ManageCourse from "./screens/ManageCourse";
 import RecentCourses from "./screens/RecentCourses";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { AntDesign } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 const CourseOverView = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="RecentCourses" component={RecentCourses} />
-      <Tab.Screen name="AllCourses" component={AllCourses} />
+    <Tab.Navigator
+      screenOptions={({ navigation }) => ({
+        headerStyle: { backgroundColor: "pink" },
+        headerTintColor: "white",
+        tabBarStyle: { backgroundColor: "pink" },
+        tabBarActiveTintColor: "darkblue",
+        headerRight: () => {
+          return (
+            <Pressable
+              onPress={() => navigation.navigate("ManageCourse")}
+              style={({ pressed }) => pressed && styles.pressed}
+            >
+              <View style={styles.iconContainer}>
+                <AntDesign name="plus" size={24} color="white" />
+              </View>
+            </Pressable>
+          );
+        },
+      })}
+    >
+      <Tab.Screen
+        name="RecentCourses"
+        component={RecentCourses}
+        options={{
+          title: "Yakın Zamanda Kaydolunanlar",
+          tabBarLabel: "Yakın Zamanda",
+          tabBarIcon: ({ color, size }) => {
+            return <AntDesign name="hourglass" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="AllCourses"
+        component={AllCourses}
+        options={{
+          title: "Tüm Kurslar",
+          tabBarLabel: "Tüm Kurslar",
+          tabBarIcon: ({ color, size }) => {
+            return <Entypo name="list" size={size} color={color} />;
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 };
@@ -33,10 +74,9 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  iconContainer: {
+    marginHorizontal: 8,
+    marginVertical: 2,
   },
+  pressed: { opacity: 0.5 },
 });
